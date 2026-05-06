3b5fbf595842a0a82f97c2a42425f10d5ed07803
gozeszn <preciousonyenaucheya2003@gmail.com>
2026-05-07 00:54:09 +0100

    new code

---
+++ new.js
@@ -0,0 +1,10 @@
+const name = "Chigozirim Onyenaucheya";
+const country = "England";
+let age = 22;
+let isStudent = true;
+let favoriteFood = null;
+console.log("My Name is " + name," I am" + age , "My Country is" + country, "my favorite food is"+  favoriteFood );
+
+// the slice function, it takes the number of characters or objects or arrays in a variable. ie let word = JavaScript, if we say slice(0,4); only 4 charcters are chosen, starting from index 0. it'll be Java as the output
+
+//in splice, if we have an array of [5,6,7,8] and splice(1,3). we start at index 1 and remove 3 characters in the array. removed is 6,7,8 while 5 is the new array.
